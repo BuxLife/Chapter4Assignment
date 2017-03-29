7d36ce7cb58bebf4ccb1e6ad3e0e2fe2f7b42cfd
BuxLife <Naeembux@live.com>
2017-03-30 01:15:00 +0200

    Inheritance & Polymorphism in Employee -Tested

--- src/main/java/com/buxlife/chapter4assignment/Employee.java
+++ src/main/java/com/buxlife/chapter4assignment/Employee.java
@@ -5,11 +5,13 @@ public class Employee extends Person{
     private int employeeID;
     private double salary;  //Per Month.
 
-    public Employee(String firstName, String lastName, String gender, int age, double salary)
+    public Employee(int id, String firstName, String lastName, String gender, int age, double salary)
     {
         super(firstName, lastName, gender, age);
         System.out.println("Constructor Initialized, Creating Employee.");
         setSalary(salary);
+        setEmployeeID(id);
+        super.setEmploymentStatus(true);
     }
 
     public void setEmployeeID(int employeeID) {
@@ -31,7 +33,13 @@ public class Employee extends Person{
     }
 
     public double calculateAnnum(){
-        System.out.println("Employee:  " + getFirstName() + "\nSalary: R" + (salary *12) );
+        System.out.println("Employee:  " + super.getFirstName() + "\nSalary: R" + (salary *12) );
         return (salary *12);
     }
+
+    public String toString()
+    {
+        return ("Employee Details: \nFirst Name: \t" + getFirstName() + "\nLast Name: \t\t" + getLastName() + "\nGender: \t\t" + getGender() + "\nAge: \t\t\t" + getAge());
+    }
+
 }
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,5 +1,8 @@
 
 import com.buxlife.chapter4assignment.Employee;
+import com.buxlife.chapter4assignment.Person;
+import org.junit.Assert;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -9,11 +12,44 @@ import static org.junit.Assert.*;
 public class EmployeeTest {
     @org.junit.Test
     public void calculateAnnum() throws Exception {
-        Employee emp1 = new Employee("John", "Smith", "Male", 21, 4500.00);
+        Employee emp1 = new Employee(9, "John", "Smith", "Male", 21, 4500.00);
         assertEquals(54000.0, emp1.calculateAnnum(), 0.0f);
     }
 
-    public void testClass() throws Exception    {
+    @Test
+    public void testEncapsulation() throws Exception {
+        Employee emp = new Employee(01,"John", "Doe", "Male", 22, 9000);
+        System.out.println(emp.toString());
+
+        emp.setFirstName("Hairy");
+        emp.setLastName("Porter");
+        emp.setGender("Female");
+        emp.setAge(18);
+        emp.setEmployeeID(99);
+        emp.setSalary(12000);
+        emp.setEmploymentStatus(true);
+        System.out.println(emp.toString());
 
     }
+
+    @Test
+    public void testPolymorphism() throws Exception    {
+        Person emp = new Employee(01,"John", "Doe", "Male", 22, 9000);
+
+        Assert.assertTrue(emp instanceof  Person);
+        Assert.assertTrue(emp instanceof  Employee);
+        System.out.println(emp.toString());
+    }
+    @Test
+    public void testInheritance() throws Exception{
+        Employee emp1 = new Employee(02, "John", "Smith", "Male", 21, 4500.00);
+        Person emp2 = new Employee(01,"John", "Doe", "Male", 22, 9000);
+
+        System.out.println(emp1 instanceof Person);
+        Assert.assertTrue(emp1 instanceof  Person);
+        Assert.assertTrue(emp2 instanceof  Person);
+        System.out.println(emp2 instanceof Person);
+
+    }
+
 }
